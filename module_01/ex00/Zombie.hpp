66d4a6eb40ba74996151c62ce36279d0cf8a5fa2
Anass Elboussaili <aelbouss@c4r5p3.1337.ma>
2025-11-29 17:08:59 +0100

    start working  on module 01

---
+++ module_01/ex00/Zombie.hpp
@@ -0,0 +1,22 @@
+
+# ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+// headers section 
+# include <iostream>
+# include <string>
+
+// classes section
+
+class   Zombie
+{
+    private:
+        std::string name;
+    public:
+    Zombie( std::string );
+    ~Zombie();
+    void    announce ( void )
+}
+
+
+# endif
