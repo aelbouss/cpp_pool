f1719ee6cd57fa2094b04dae278654ab60f6ba0e
aelbouss <elboussailianass37@gmail.com>
2025-12-08 15:59:27 +0100

    module_01 done

--- module_01/ex06/main.cpp
+++ module_01/ex06/main.cpp
@@ -6,7 +6,10 @@ int main(int ac, char **av)
     std::string str;
 
     if (ac != 2)
+    {
         std::cerr << "<usage> : ./ProgramName  Arg" << std::endl;
+        return (1);
+    }
     str = av[1];
     comp.complain(str);
     return (0);
