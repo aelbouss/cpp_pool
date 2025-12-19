73d91d5f50821c42290c7333e22856f5eda019ef
Anass Elboussaili <aelbouss@c4r4p1.1337.ma>
2025-12-19 23:48:11 +0100

    done module_02/ex02

--- module_02/ex02/Fixed.cpp
+++ module_02/ex02/Fixed.cpp
@@ -8,12 +8,12 @@ Fixed::Fixed() // default constructor
 
 Fixed::Fixed( const int nbr) // constructor builds int
 {
-	setRawBits(roundf(nbr * pow(2, scaling_factor)));
+	setRawBits(roundf(nbr * (1 << scaling_factor)));
 }
 
 Fixed::Fixed( const float nbr) // constructor builds float
 {
-	setRawBits(roundf(nbr * pow(2, scaling_factor)));
+	setRawBits(roundf(nbr * (1 << scaling_factor)));
 }
 
 Fixed& Fixed::operator = ( const Fixed& src) // copy assignment  operator
@@ -46,16 +46,128 @@ void Fixed::setRawBits( int const raw )
 
 float Fixed::toFloat( void ) const
 {
-	return (static_cast <float>(fixedpoint) / pow(2, scaling_factor));
+	return (static_cast <float>(fixedpoint) / (1 << scaling_factor));
 }
 
 int	Fixed::toInt( void ) const
 {
-	return (fixedpoint / pow(2, scaling_factor));
+	return (fixedpoint / (1 << scaling_factor));
 }
 
 std::ostream&	operator << ( std::ostream& os , const Fixed &src)
 {
-	std::cout << src.toFloat();
+	os << src.toFloat();
 	return (os);
+}
+
+Fixed	Fixed::operator + (const Fixed& src) const
+{
+	return (this->toFloat() + src.toFloat());
+}
+
+Fixed	Fixed::operator - (const Fixed& src) const
+{
+	return (this->toFloat() - src.toFloat());
+}
+
+
+Fixed	Fixed::operator * (const Fixed& src) const
+{
+	float fv;
+	float sv;
+
+	fv = this->toFloat();
+	sv = src.toFloat();
+ 	return (fv * sv);
+}
+
+Fixed	Fixed::operator / (const Fixed& src) const
+{
+	return (this->fixedpoint / src.getRawBits());
+} 
+
+bool	Fixed::operator < (const Fixed& src) const
+{
+	return (this->getRawBits() < src.getRawBits());
+}
+
+bool	Fixed::operator > (const Fixed& src) const
+{
+	return (this->getRawBits() > src.getRawBits());
+}
+
+bool	Fixed::operator != (const Fixed& src) const
+{
+	return (this->getRawBits() != src.getRawBits());
+}
+
+bool	Fixed::operator == (const Fixed& src) const
+{
+	return (this->getRawBits() == src.getRawBits());
+}
+
+bool	Fixed::operator >= (const Fixed& src) const
+{
+	return (this->getRawBits() >= src.getRawBits());
+}
+
+bool	Fixed::operator <= (const Fixed& src) const
+{
+	return (this->getRawBits() <= src.getRawBits());
+}
+
+
+Fixed	Fixed::operator ++ (int) // postfix increment
+{
+	Fixed	obj(*this);
+	fixedpoint++;
+	return (obj);
+}
+
+Fixed	Fixed::operator ++() // prefexi incement
+{
+	fixedpoint++;
+	return (*this);
+}
+
+Fixed	Fixed::operator --(int) // postfix decrement
+{
+	fixedpoint--;
+	return (*this);
+}
+
+Fixed	Fixed::operator --() // prefix  decrement
+{
+	Fixed	obj(fixedpoint);
+
+	fixedpoint--;
+	return (obj);
+}
+
+Fixed&	Fixed::min(Fixed& first, Fixed& second)
+{
+	if (first < second)
+		return (first);
+	return (second);
+}
+
+const Fixed&	Fixed::min(const Fixed& first, const Fixed& second)
+{
+	if (first < second)
+		return (first);
+	return (second);
+}
+
+Fixed&	Fixed::max(Fixed& first, Fixed& second)
+{
+	if (first > second)
+		return (first);
+	return (second);
+}
+
+const Fixed&	Fixed::max(const Fixed& first, const Fixed& second)
+{
+	if (first > second)
+		return (first);
+	return (second);
 }
