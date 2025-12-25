83a3a4e03174bef84180213a2479a779d528c267
aelbouss <elboussailianass37@gmail.com>
2025-12-25 18:26:55 +0100

    done  module_03/ex00

---
+++ module_03/ex00/ClapTrap.cpp
@@ -0,0 +1,57 @@
+# include "ClapTrap.hpp"
+
+
+ClapTrap::ClapTrap(std::string name) : Name(name) 
+{
+	hit_points = 10;
+	Energy_points = 10;
+	Attack_dammage = 0;
+	std::cout << BLUE << "Constructor Called"<< RESET << std::endl;
+}
+
+void ClapTrap::attack(const std::string& target)
+{
+	if (hit_points == 0)
+	{
+		std::cout << this-> Name << RED << "is dead" << RESET << std::endl;
+		return;
+	}
+	if (Energy_points == 0)
+	{
+		std::cout << RED << "0 Energy Points"<< RESET << std::endl;
+		return ;
+	}
+	std::cout << RED << "ClapTrap "<< YELLOW << this->Name << RED << " attacks "<< YELLOW << target << RED << " , causing "
+		<< hit_points << " Points of damage" << RESET<< std::endl;
+	this->Energy_points--;
+}
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << BLUE << "Destructor Called" << BLUE << std::endl;
+}
+
+void	ClapTrap::takeDammage(unsigned int amount)
+{
+	std::cout << YELLOW << this->Name << RED << " get damaged by : " << amount << "Damage Points" <<  RESET << std::endl;
+	if (amount > (unsigned int)this->hit_points)
+	{
+		this->hit_points = 0;
+		return ;
+	}
+	this->hit_points -= amount;
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (hit_points == 0)
+	{
+		std::cout<< YELLOW << this->Name << RED << "is dead" << std::endl;
+		return ;
+	}
+	if (Energy_points == 0)
+		return ;
+	hit_points += amount;
+	std::cout << GREEN << this->Name << " get repairs itself by : " << amount << std::endl;
+	this->Energy_points--; 
+}
---
+++ module_03/ex00/main.cpp
@@ -0,0 +1,16 @@
+# include  "ClapTrap.hpp"
+
+
+int	main(void)
+{
+	ClapTrap	c1("anass");
+	ClapTrap	c2("wins");
+
+	c1.attack("wins");
+	c2.takeDammage(3);
+	c2.attack("anass");
+	c1.takeDammage(10);
+	c1.beRepaired(4);
+	c2.beRepaired(5);
+
+}
