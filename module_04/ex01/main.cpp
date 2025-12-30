90f2d4e22245337016fee4a1f29b72061a9e1470
aelbouss <elboussailianass37@gmail.com>
2025-12-30 02:31:04 +0100

    done module_04/ex01

--- module_04/ex01/main.cpp
+++ module_04/ex01/main.cpp
@@ -34,6 +34,14 @@ int main()
 		delete cat2;
 	}
 
+	{
+		const Animal* j = new Dog();
+		const Animal* i = new Cat();
+		delete j; //should not create a leak
+		delete i;
+	}
+	
+
 
 	return 0;
 } 
